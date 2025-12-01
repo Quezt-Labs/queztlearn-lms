7c9f154179eedec0aeaff961c506d9c169ae6e23
imshiv4m <shivamjha190@gmail.com>
2025-12-01 21:40:42 +0530

    feat: add @vercel/speed-insights for performance monitoring
    - Integrated the @vercel/speed-insights package into the layout component to enhance performance tracking.
    - Updated package.json and pnpm-lock.yaml to include the new dependency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { Analytics } from "@vercel/analytics/next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,6 +25,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
